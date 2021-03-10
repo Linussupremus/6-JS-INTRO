93ce1602f9f6dbb3e57524adf4566f3fb633c233
Linas <linus.supremus@gmail.com>
2021-03-10 07:42:43 +0200

    commit

---
+++ js/00-notes.js
@@ -0,0 +1,5 @@
+// kintamieji - jo atmintyje yra laikoma informacija;
+// sąlygos sakiniai - išsakoti logiką pagal ką daryti kai daugiau/mažiau, tiesa/netiesa.
+// ciklai - kai turime sąrašą su vienoda informaciją kuri yra vienodos struktūros.
+// funkcijos - aprašome logiką, kurią norime perpanaudoti N kartų, tik su galimai skirtingais parametrais.
+// event'ai (event listeners) - skirta stebėti įvykius( clikc, scroll, keypress ir pan) į kuriuos reikia sureaguoti iškviečiant norimą funkciją.
